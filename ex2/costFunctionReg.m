94987eec48982e96dfefe6d96cef4dde3b7ad04f
Calin <calinoiu.alexandru@agilefreaks.com>
2016-07-31 23:17:39 +0300

    regulazing logistic regresion gradient

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -9,9 +9,10 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 h = sigmoid(X * theta);
-reg = (lambda * (ones(1, length(theta) - 1) * theta(2:length(theta),:).^2)) / (2*m);
+meningfullTheta = theta(2:length(theta),:);
+reg = (lambda * (ones(1, length(theta) - 1) * meningfullTheta.^2)) / (2*m);
 J = (-y' * log(h) - (ones(m, 1) - y)' * log(ones(m, 1) - h)) / m + reg;
-grad = ((h - y)' * X) / m;
+grad = ((h - y)' * X) / m + [0, (lambda .* meningfullTheta') ./ m];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
